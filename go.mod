15633e01641c69a01766949d2d6cf49ee066db29
mayankxjha <mayankxjha@gmail.com>
2025-11-05 11:30:48 +0530

    chore: day1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mayankxjha/golang-9
+
+go 1.25.3
---
+++ hello-world.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println(strings.Repeat("-", 50))
+	values()
+	fmt.Println(strings.Repeat("-", 50))
+	variables()
+}
